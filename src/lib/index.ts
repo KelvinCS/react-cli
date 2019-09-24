5d67bb6e626812b2070db95483a6dcd83875e95f
Kelvin CS <kelvinh.job@gmail.com>
2019-09-23 22:57:10 -0300

    Criando funções de auxilio a command line

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,8 @@
 import fs from 'fs';
 import path from 'path';
-import { includes, __, partial, equals } from 'ramda';
+import { promisify } from 'util';
+import { includes, __, partial, equals, isNil, complement } from 'ramda';
+import { exec, ExecOptions } from 'child_process';
 
 export type ComponentProps = {
   type: string;
@@ -13,8 +15,23 @@ export type Component = {
 };
 
 /**
+ * Executes a command and writes the output to process.stdin
+ * @param command - Command to be executed
+ * @param callback
+ */
+const execWithOutput = (command: string, options?: ExecOptions, callback?) =>
+  exec(command, options, callback).stdout.pipe(process.stdout);
+
+/**
+ * Executes a command on the current terminal
+ * @param command - Command to be executed
+ */
+export const executeCommand = (command: string, options?: ExecOptions) =>
+  promisify(execWithOutput)(command, options);
+
+/**
  * Receives a predicate and retuns a funcion who receives
- * a ComponentProps, and compare the `type` field with de predicate
+ * a para of type ComponentProps, and compare the field `type` with de predicate
  * @param pred
  */
 export const ofType = (pred: string) => ({ type }) => equals(pred, type);
@@ -26,8 +43,7 @@ export const getCurrentPath = () => process.cwd();
 
 /**
  * Receives a list and returns a verifier funcion that checks
- * if the param exists on the given list
- *
+ * if the param exists in the given list
  * @param list
  */
 export const oneOf = (list: any[]) => (param: any) => includes(param, list);
@@ -46,20 +62,30 @@ export const bindWith = (fn: () => any, ...params: any) => partial(fn, params);
 export const getSrcDirPath = () => path.join(getCurrentPath(), 'src');
 
 /**
+ * TODO
+ * Recursively create a file
+ * @param path
+ * @param data
+ */
+export function createFileRecursive(dirPath: string, data: any) {
+  const createDirs = (dirPath) => {};
+}
+
+/**
  * Create a file on disk
  * @param path path where to write the file
  * @param data content of the file
  */
-export function createFile(path: string, data: any) {
-  return new Promise((resolve) => fs.writeFile(path, data, resolve));
-}
+export const createFile = promisify(fs.writeFile);
 
 /**
  * Create a dir on disk
  * @param path path where to write the dir
  */
-export function createDir(path: string) {
-  return new Promise((resolve) =>
-    fs.mkdir(path, null, bindWith(resolve, path))
-  );
-}
+export const createDir = promisify(fs.mkdir);
+
+/**
+ * Verifies if a value is not nill
+ * @param value value to be compared
+ */
+export const notNil: (value: any) => boolean = complement(isNil);
